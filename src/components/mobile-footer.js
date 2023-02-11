ea4425d9e9a8bab58bf13556acbe1bd83d42bf34
latenightluke <luke.ivie@icloud.com>
2023-02-10 19:16:47 -0700

    style: bottom nav finally

--- src/components/mobile-footer.js
+++ src/components/mobile-footer.js
@@ -3,7 +3,7 @@ import MobileMenuItem from "./mobile-menu-item";
 
 export default function MobileFooter({ menuItems }) {
   return (
-    <footer className="fixed bottom-0 lg:hidden text-white bg-black w-full flex">
+    <footer className="fixed bottom-0 lg:hidden bg-black border-t-2 border-gray-700 w-full flex p-2">
       {menuItems?.map((item) => (
         <MobileMenuItem key={item?.name} to={item?.pathname} icon={item?.icon}>
           {item?.name}
--- src/components/mobile-menu-item.js
+++ src/components/mobile-menu-item.js
@@ -10,7 +10,14 @@ export default function MobileMenuItem({ children, icon, to }) {
   return (
     <Link
       href={to}
-      className="bg-black border-t-2 border-gray-700 group flex flex-col flex-grow justify-center items-center text-xl pt-3 pb-5"
+      className={cn(
+        "bg-black group flex flex-col flex-grow justify-center items-center rounded-m py-2 px-3 rounded-full",
+        {
+          "text-white": active,
+          "text-gray-600 group-hover:text-gray-400": !active,
+          "bg-indigo-600": active,
+        }
+      )}
     >
       {/* <FontAwesomeIcon
         icon={[active ? "fas" : "fal", icon]}
@@ -20,16 +27,7 @@ export default function MobileMenuItem({ children, icon, to }) {
             : "text-gray-600 group-hover:text-indigo-400"
         } text-2xl mb-2`}
       /> */}
-      <span
-        className={cn("text-base border-b-2", {
-          "text-white": active,
-          "text-gray-600 group-hover:text-gray-400": !active,
-          "border-white": active,
-          "border-transparent": !active,
-        })}
-      >
-        {children}
-      </span>
+      <span>{children}</span>
       {/* <span
         className={cn("w-[6px] h-[6px] rounded-full mt-1", {
           "bg-white": active,
