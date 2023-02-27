969f55b65814ac7d36dc43512bf78e7b9fa6f2c7
latenightluke <luke.ivie@icloud.com>
2023-02-27 00:11:53 -0700

    style: slight tweaks to nav styles

--- src/components/menu-item.tsx
+++ src/components/menu-item.tsx
@@ -20,14 +20,12 @@ export default function MenuItem({ children, icon, to }: Props) {
     <Link
       href={to}
       className={cn(
-        "text-center block font-semibold text-base md:text-lg px-5 py-2 rounded-full",
-        // "border-y-2 md:border-y"
+        "text-center block font-semibold text-base md:text-lg px-5 py-2 rounded-full transition-colors duration-200",
         {
-          "dark:text-white dark:border-b-slate-700 dark:border-t-slate-500": active,
-          "text-white bg-neutral-900 dark:bg-slate-800 dark:text-white": active,
-          "dark:text-slate-600 dark:hover:text-slate-400 dark:active:text-white": !active,
-          "border-transparent transition-colors duration-200": !active,
+          "text-white bg-neutral-900": active,
+          "dark:text-white dark:bg-slate-800": active,
           "text-neutral-500 hover:text-neutral-800 active:text-neutral-900": !active,
+          "dark:text-slate-500 dark:hover:text-slate-400 dark:active:text-white": !active,
         }
       )}
     >
