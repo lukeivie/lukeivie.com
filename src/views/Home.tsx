e792106dd8318fc91b0078bab8163027121795d5
latenightluke <luke.ivie@icloud.com>
2023-02-26 23:44:52 -0700

    cleanup: remove old assets and rename

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Head from "next/head";
 import Image from "next/image";
-import profileImage from "../assets/lnl.jpg";
+import profileImage from "../assets/profile-image.jpg";
 import { Transition } from "@headlessui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin, faTwitter } from "@fortawesome/free-brands-svg-icons";
